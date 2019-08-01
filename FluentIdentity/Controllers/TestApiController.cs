7454674f6820c871a3c6d327bb8a38138c2db996
Константин Лупашку <dreadpat@gmail.com>
2019-08-01 13:20:40 +0300

    Подключен swagger

---
+++ FluentIdentity/Controllers/TestApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FluentIdentity.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TestApiController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult TestGet()
+		{
+			return Ok();
+		}
+	}
+}
--- FluentIdentity/Startup.cs
+++ FluentIdentity/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using FluentIdentity.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace FluentIdentity
 {
@@ -43,6 +44,11 @@ namespace FluentIdentity
 				.AddEntityFrameworkStores<ApplicationDbContext>();
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new Info { Title = "FluentIdentity API", Version = "v1" });
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,12 +72,21 @@ namespace FluentIdentity
 
 			app.UseAuthentication();
 
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "FluentIdentity API V1");
+			});
+
+
 			app.UseMvc(routes =>
 			{
 				routes.MapRoute(
 					name: "default",
 					template: "{controller=Home}/{action=Index}/{id?}");
 			});
+
+			
 		}
 	}
 }
